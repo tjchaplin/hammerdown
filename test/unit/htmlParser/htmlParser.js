cb73191a830957266d8f41a46d79898521919fad
tjchaplin <tjchaplin@hotmail.com>
2014-04-10 23:22:12 +0100

    Updated to allow appending to stream during the reading of the html file

---
+++ test/unit/htmlParser/htmlParser.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var HtmlParser = require('../../../lib/htmlParser/htmlParser');
+var stringToStream = require('../../../lib/utils/stringToStream');
+
+describe("When parsing an html file",function(){
+	var htmlParser = new HtmlParser();
+
+	describe("when listening for depth events",function(){
+		var depthData =  [];
+		htmlParser.on('tagdepth',function(depth){
+			depthData.push(depth);
+		});
+		describe("on open tag",function(){
+			stringToStream("<html><h1></h1><h2></h2></html>").pipe(htmlParser);
+			it("tagdepth should be emitted for each open and close",function(){
+				assert(depthData.length === 4);
+			});
+			it("tagdepth should emit in correct sequence",function(){
+				console.log(depthData);
+				var expectedDepthData = [1,0,1,0];
+				for (var i = 0; i < expectedDepthData.length; i++) {
+					assert(expectedDepthData[i] === depthData[i]);
+				};
+				
+			});
+		});
+	});
+});

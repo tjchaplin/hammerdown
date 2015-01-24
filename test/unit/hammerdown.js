101e019d153c61fc856aceb74aa0cd8ce0c6b171
tjchaplin <tjchaplin@hotmail.com>
2015-01-24 00:51:41 +0000

    Added missing out files

--- test/unit/hammerdown.js
+++ test/unit/hammerdown.js
@@ -4,8 +4,11 @@ var hammerdown = require('../../lib/hammerdown');
 
 describe('When using hammerdown with a string',function(){
 	it('should produce expected markdown',function(){
-		hammerdown().parse('<h1>Header</h1>').pipe(concat(function(data){
-			assert(data == '# Header');
-		}));
+		hammerdown().parse('<p><!--Current comment--><a href="#someclass">#</a><span> | </span><!--file table of  contents--><a href="#methods">&#x24C9;</a></p>').pipe(process.stdout);
 	});
+	// it('should produce expected markdown',function(){
+	// 	hammerdown().parse('<h1>Header</h1>').pipe(concat(function(data){
+	// 		assert(data == '# Header');
+	// 	}));
+	// });
 });

a226519494c7cd0003f3c353cf9f46ed28b41c79
tjchaplin <tjchaplin@hotmail.com>
2015-01-27 22:27:02 +0000

    Removed unused files

--- bower_components/hammerdown/lib/utils/stringToStream.js
+++
@@ -1,9 +0,0 @@
-var Readable = require('stream').Readable;
-
-module.exports = function stringToStream(string){
-    var rs = new Readable();
-    rs.push(string);
-    rs.push(null);
-    
-    return rs;
-};
